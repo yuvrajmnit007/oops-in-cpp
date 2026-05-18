320de6c0311b2216ec4e2d4d4ce6b8501f6451fd
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-05-18 18:53:41 +0530

    basic geometry functions using class and objects

---
+++ example1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        
+    }
+    return 0;
+}
